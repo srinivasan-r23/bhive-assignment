3f57c436a163824df5c74672caa2a532930996e4
Srini <srinismart2@gmail.com>
2024-12-27 19:12:29 +0530

    download app section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,26 +1,10 @@
-import React from "react";
 import { FEATURES_SECTION } from "../utils/constants";
 import Heading from "./Heading";
 
 const Features = () => {
   return (
     <section className="text-[#263238] pt-[50px] lg:pt-9 lg:pb-10 pb-[30px]">
-      <div className="flex items-center justify-between">
-        <Heading text={"Why Choose us?"} />
-        <svg
-          className="lg:hidden"
-          width="25"
-          height="8"
-          viewBox="0 0 25 8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M24.3536 4.35355C24.5488 4.15829 24.5488 3.84171 24.3536 3.64645L21.1716 0.464466C20.9763 0.269204 20.6597 0.269204 20.4645 0.464466C20.2692 0.659728 20.2692 0.976311 20.4645 1.17157L23.2929 4L20.4645 6.82843C20.2692 7.02369 20.2692 7.34027 20.4645 7.53553C20.6597 7.7308 20.9763 7.7308 21.1716 7.53553L24.3536 4.35355ZM0 4.5H24V3.5H0V4.5Z"
-            fill="#FFBB00"
-          />
-        </svg>
-      </div>
+      <Heading text={"Why Choose us?"} />
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-0 mt-[30px] lg:mt-10">
         {FEATURES_SECTION.map((section, index) => (
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#222E34] text-center py-3 text-[#dddddd] w-full mt-auto">
+    <footer className="bg-[#222E34] text-center py-3 text-[#dddddd] w-full mt-auto text-xs lg:text-sm">
       © Copyright 2024. Bhive Private Limited
     </footer>
   );
