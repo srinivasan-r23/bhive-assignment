214e172d0fbeaab7fc6a76c9433ff0177f6976ff
Srini <srinismart2@gmail.com>
2024-12-28 00:57:55 +0530

    hero banner added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,22 @@ const Hero = () => {
   return (
     <section>
       <div className="relative">
-        <img
-          src="https://booking-stag.bhiveworkspace.com/assets/HeroDesktop-dd965ebf.svg"
-          alt="Bhive Workspace"
-          className="w-full "
-        />
-        <h1 className="font-semibold lg:font-bold text-xl lg:leading-[70px] lg:text-[58px] text-[#263238] text-center md:text-start relative lg:top-1/4 lg:absolute lg:w-1/2">
+        <picture>
+          <source
+            media="(max-width: 767px)"
+            srcSet="https://booking-stag.bhiveworkspace.com/assets/HeroMobile-041216cf.svg"
+          />
+          <source
+            media="(min-width: 768px)"
+            srcSet="https://booking-stag.bhiveworkspace.com/assets/HeroDesktop-dd965ebf.svg"
+          />
+          <img
+            src="https://booking-stag.bhiveworkspace.com/assets/HeroDesktop-dd965ebf.svg"
+            alt="Hero"
+            className="w-full"
+          />
+        </picture>
+        <h1 className="font-semibold md:font-bold text-xl md:leading-[70px] md:text-[40px] lg:text-[58px] text-[#263238] text-center md:text-start relative md:top-1/4 md:absolute md:w-[70%]">
           {"Host your meeting with world-class amenities. Starting at "}
           <span className="text-[#FFBB00]">{"₹199/-!"}</span>
         </h1>
