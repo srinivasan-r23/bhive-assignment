99f8580e79ed54795803e7ee83266ca7bd1a86f7
Srini <srinismart2@gmail.com>
2024-12-27 15:06:27 +0530

    Project setup - Tailwind and Inter font

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="font-bold text-4xl">
+      Host your meeting with world-class amenities. Starting at ₹199/-!
+    </h1>
+  );
+}
+
+export default App;
